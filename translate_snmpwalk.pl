99a94e64869f200b60b501ac9ffd9fb27050b6ee
Keith Sinclair <keiths@opmantek.com>
2020-01-29 13:19:46 +1000

    Updated translate to handle full OID's in input

--- translate_snmpwalk.pl
+++ translate_snmpwalk.pl
@@ -20,6 +20,9 @@ use strict;
 # change this if you don't store the file here.
 my $oidfile = "/usr/local/mibs/mibs.oid";
 my $enterprisePrefix = "1.3.6.1.4.1";
+my $stardardPrefix = "1.3.6.1.2";
+my $snmpV2Prefix = "1.3.6.1.6";
+my $needsTranslate = qr/(SNMPv2-SMI::enterprises.\d+|DPS-MIB-V38::dpsAlarmControl.\d+|SNMPv2-SMI::mib-2.\d+|SNMPv2-SMI::snmpModules.\d+)/;
 
 &main;
 
@@ -59,30 +62,37 @@ sub main {
 
 		#SNMPv2-SMI::enterprises.14706.1.1.4.1.5.1 = INTEGER: 1
 		#compile the regex so it is super fast
-		my $needsTranslate = qr/SNMPv2-SMI::enterprises.\d+/;
+		
 
-        open (IN, $file) or die "Cannot open $file. $!";
-        while (<IN>) {
-        	++$lines;
-        	chomp;
+		open (IN, $file) or die "Cannot open $file. $!";
+		while (<IN>) {
+			++$lines;
+			chomp;
 
 			#get the OID into a variable
 			my ($oid,$rest) = split(" = ",$_);
 
-        	if ( $oid =~ /$needsTranslate/ ) {
-				++$translated;
+	        if ( $oid =~ /$needsTranslate/ ) {
 				my $oidTranslated = 0;
 
 				# get partial translated back to full OID
 				$_ =~ s/SNMPv2-SMI::enterprises/1.3.6.1.4.1/g;
 				$oid =~ s/SNMPv2-SMI::enterprises/1.3.6.1.4.1/g;
 
+				# handling the DPS-MIB-V38::dpsAlarmControl MIB
+				$_ =~ s/DPS-MIB-V38::dpsAlarmControl/1.3.6.1.4.1.2682.1/g;
+				$oid =~ s/DPS-MIB-V38::dpsAlarmControl/1.3.6.1.4.1.2682.1/g;
+
+				$_ =~ s/SNMPv2-SMI::mib-2/1.3.6.1.2.1/g;
+				$oid =~ s/SNMPv2-SMI::mib-2/1.3.6.1.2.1/g;
+
+				$_ =~ s/SNMPv2-SMI::snmpModules/1.3.6.1.6.3/g;
+				$oid =~ s/SNMPv2-SMI::snmpModules/1.3.6.1.6.3/g;
+
 				# the oid finishes in something other than 0, probably a simple table index.
 				if ( $oid =~ /\.\d+$/ ) {
 					$oid =~ s/\.\d+$//;
 					if ( defined $mibs->{$oid} and $mibs->{$oid} ne "" ) {
-						$oidTranslated = 1;
-						$_ =~ s/$oid/$mibs->{$oid}/;
 					}
 				}
 
@@ -90,7 +100,8 @@ sub main {
 				# 1.3.6.1.4.1.3375.2.2.14.6.3.1.30.16.47.67.111.109.109.111.110.47.76.83.78.95.80.111.111.108
 				# "ltmLsnPoolStatTotalPortBlockDeallocations"			"1.3.6.1.4.1.3375.2.2.14.6.3.1.30"
 				if ( not $oidTranslated ) {
-					# keep processing the oid till it gets to the Enterprise Prefix
+					# keep processing the oid till it gets to the Enterprise
+					# stop processing when we get to the enterprise prefix.
 					while ( $oid ne $enterprisePrefix and $oid ne "SNMPv2-SMI::enterprises" ) {
 						$oid = shortenOid($oid);
 						if ( defined $mibs->{$oid} and $mibs->{$oid} ne "" ) {
@@ -102,22 +113,47 @@ sub main {
 				}
 
 				if ( $oidTranslated ) {
+					++$translated;
 					print "$_\n";
 				}
 				else {
 					# this means missing a MIB or not handling the index yet.
-        			print "NEEDS TRANSLATE: $_\n";
-        		}
-        	}
-        	else {
-        		print "$_\n";
-        	}
-        }   
-        print "$lines lines processed, $translated lines translated\n";     
-    }
-    else {
-    	print "ERROR, problem with $file, can not read it.\n";
-    }
+					print "NEEDS TRANSLATE: $_\n";
+				}
+			}
+	        elsif ( $oid =~ /$stardardPrefix|$snmpV2Prefix|$enterprisePrefix/ ) {
+	        	# remove the leading . from the oid
+	        	$oid =~ s/^\.//;
+				my $oidTranslated = 0;
+				while ( $oid ne $stardardPrefix and $oid ne $snmpV2Prefix and $oid ne $enterprisePrefix ) {
+					$oid = shortenOid($oid);
+					if ( defined $mibs->{$oid} and $mibs->{$oid} ne "" ) {
+						$oidTranslated = 1;
+						$_ =~ s/$oid/$mibs->{$oid}/;
+						# remove the leading . from the line.
+						$_ =~ s/^\.//;
+						last;
+					}
+				}
+				if ( $oidTranslated ) {
+					++$translated;
+					print "$_\n";
+				}
+				else {
+					# this means missing a MIB or not handling the index yet.
+					print "NEEDS TRANSLATE: $_\n";
+				}
+
+			}
+			else {
+				print "$_\n";
+			}
+		}   
+		print "$lines lines processed, $translated lines translated\n";     
+	}
+	else {
+		print "ERROR, problem with $file, can not read it.\n";
+	}
 }
 
 sub shortenOid {
